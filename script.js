8a60fd6b7c7849368d96d9d41e466669f8b32a94
Dustin Durden <23153218+dkdurden@users.noreply.github.com>
2022-11-08 20:55:03 -0500

    Setup project html layout, begin adding styles and JS

---
+++ script.js
@@ -0,0 +1,39 @@
+function addEvents() {
+  const dropdowns = document.querySelectorAll(".dropdown");
+  const navToggle = document.querySelector(".nav__toggle");
+
+  dropdowns.forEach((dropdown) => {
+    dropdown.addEventListener("click", (e) => {
+      const menu = e.currentTarget.lastElementChild;
+      const isOpen = menu.classList.contains("show");
+
+      console.log(menu);
+
+      if (isOpen) {
+        menu.classList.remove("show");
+      } else {
+        menu.classList.add("show");
+      }
+    });
+  });
+
+  navToggle.addEventListener("click", (e) => {
+    const expanded = e.currentTarget.getAttribute("aria-expanded") === "true";
+    const navMenu = e.currentTarget.nextElementSibling;
+
+    console.log(expanded, navMenu);
+
+    if (!expanded) {
+      e.currentTarget.setAttribute("aria-expanded", "true");
+      e.currentTarget.innerHTML =
+        '<img src="/images/icon-close-menu.svg" alt="" />';
+      navMenu.style.display = "block";
+    } else {
+      e.currentTarget.setAttribute("aria-expanded", "false");
+      e.currentTarget.innerHTML = '<img src="/images/icon-menu.svg" alt="" />';
+      navMenu.style.display = "none";
+    }
+  });
+}
+
+addEvents();
