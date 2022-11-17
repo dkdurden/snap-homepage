a89febad302a5a2e5f09b02ad3597e22dd92100d
Dustin Durden <23153218+dkdurden@users.noreply.github.com>
2022-11-16 20:17:46 -0500

    Add code to switch dropdown icons based on open status, add box shadow for mobile menu toggle

--- script.js
+++ script.js
@@ -1,20 +1,23 @@
 // simple JS for easy development purposes
 // TODO: improve this after styles are done, and make accessible
 function addEvents() {
-  const dropdowns = document.querySelectorAll(".dropdown");
+  const dropdownToggles = document.querySelectorAll(".dropdown__toggle");
   const navToggle = document.querySelector(".nav__toggle");
 
-  dropdowns.forEach((dropdown) => {
-    dropdown.addEventListener("click", (e) => {
-      const menu = e.currentTarget.lastElementChild;
+  dropdownToggles.forEach((toggle) => {
+    toggle.addEventListener("click", (e) => {
+      const menu = e.currentTarget.nextElementSibling;
+      const icon = e.currentTarget.lastElementChild;
       const isOpen = menu.classList.contains("show");
-
-      console.log(menu);
 
       if (isOpen) {
         menu.classList.remove("show");
+
+        icon.outerHTML = '<img src="/images/icon-arrow-down.svg" alt="" />';
       } else {
         menu.classList.add("show");
+
+        icon.outerHTML = '<img src="/images/icon-arrow-up.svg" alt="" />';
       }
     });
   });
@@ -23,16 +26,17 @@ function addEvents() {
     const expanded = e.currentTarget.getAttribute("aria-expanded") === "true";
     const navMenu = e.currentTarget.nextElementSibling;
 
-    console.log(expanded, navMenu);
-
     if (!expanded) {
       e.currentTarget.setAttribute("aria-expanded", "true");
       e.currentTarget.innerHTML =
         '<img src="/images/icon-close-menu.svg" alt="" />';
+
       navMenu.style.display = "block";
+      navMenu.style.boxShadow = "0px 0px 0px 100vw rgb(0 0 0 / 50%)";
     } else {
       e.currentTarget.setAttribute("aria-expanded", "false");
       e.currentTarget.innerHTML = '<img src="/images/icon-menu.svg" alt="" />';
+
       navMenu.style.display = "none";
     }
   });
